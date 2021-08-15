cc7ac222f90cddea46dceb49c75020e85a284d38
Matt Oliver <mattoliver.mattoliver@gmail.com>
2021-08-15 15:21:50 -0500

    boilerplate componenet :bug:

--- src/components/BoilerplateComponent.js
+++ src/components/BoilerplateComponent.js
@@ -11,7 +11,7 @@ function BoilerPlate({ data, property }) {
   // will be called initially and on every data change
   useEffect(() => {
     const svg = select(svgRef.current);
-    
+
     // use resized dimensions; instead of skipping it if dimensions are undefined,
     // we fall back to the boundingClientRect of this DOM element if the dimensions are
     // null at the very beginning
@@ -20,13 +20,12 @@ function BoilerPlate({ data, property }) {
 
   }, [data, dimensions, property])
 
-  return(
-    <div ref={wrapperRef} style={{ marginBottom: '2rem '}}>
+  return (
+    <div ref={wrapperRef} style={{ marginBottom: '2rem ' }}>
       {/*render an svg that and access it in the useEffect hook after the dom elements have been rendered*/}
       <svg ref={svgRef}></svg>
-    }
     </div>
-    )
+  )
 
 }
 
